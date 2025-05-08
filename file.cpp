a8a63f5a5ec77968864c49b059d46e8533056896
DrivandiPratama <dripandipratama@gmail.com>
2025-05-08 19:04:41 +0700

    Membuat while

--- file.cpp
+++ file.cpp
@@ -40,3 +40,21 @@ public:
             START = nodeBaru;
             return;
         }
+
+        Node *previos = START;
+        Node *current = START;
+
+        while ((current != NULL) && (nim >= current -> noMhs))
+        {
+            if (nim == current -> noMhs)
+            {
+                cout << "\nDuplikasi noMhs tidak diijinkan\n";
+                return;
+            }
+            previos = current;
+            current = current -> next;
+        }
+
+        nodeBaru -> next = current;
+        previos -> next = nodeBaru;
+    }
