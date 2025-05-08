5cb15fb073db5b2747d5be5fc2ccaad613423daf
DrivandiPratama <dripandipratama@gmail.com>
2025-05-08 19:51:51 +0700

    switch case 3

--- file.cpp
+++ file.cpp
@@ -154,3 +154,8 @@ int main()
                          << "Data dengan no mahasiswa" << nim << "berhasil dihapus" << endl;
             }
             break;
+            case '3':
+            {
+                mhs.traverse();
+            }
+            break;
